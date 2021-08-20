3e8be30a82f2fa95c53eb3af7fbbe6ea467fe2dc
Dn <skutari.daniel@gmail.com>
2021-08-20 16:01:49 +0300

    Убрал повторный вызов функции

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -2,13 +2,14 @@ import { getCities, totalPrice} from '../utils/trip';
 import AbstractView from './abstract';
 
 const createTripInfoTemplate = (data) => {
+  const {firstCity, secondCity, thirdCity, fromDate, toDate} = getCities(data);
   if (data.length === 0) {
     return ' ';
   }
   return `<section class="trip-main__trip-info  trip-info">
   <div class="trip-info__main">
-    <h1 class="trip-info__title">${getCities(data)['firstCity']} — ${getCities(data)['secondCity']} — ${getCities(data)['thirdCity']}</h1>
-    <p class="trip-info__dates">${getCities(data)['fromDate']}&nbsp;—&nbsp;${getCities(data)['toDate']}</p>
+    <h1 class="trip-info__title">${firstCity} — ${secondCity} — ${thirdCity}</h1>
+    <p class="trip-info__dates">${fromDate}&nbsp;—&nbsp;${toDate}</p>
   </div>
   <p class="trip-info__cost">
     Total: €&nbsp;<span class="trip-info__cost-value">${totalPrice(data)}</span>
